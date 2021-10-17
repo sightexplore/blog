b502333f73be3358f77f7e8645ca183a158b2347
Sight Explore <sight.explore@gmail.com>
2021-10-18 00:57:57 +0530

    Style Restored

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}', 
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
 
   darkMode: 'class', // or 'media' or 'class'

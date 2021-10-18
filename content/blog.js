2d72ce40939faa6e34ff50b0d3fc679a041de91d
Sight Explore <sight.explore@gmail.com>
2021-10-18 13:07:45 +0530

    API SET

---
+++ content/blog.js
@@ -0,0 +1,14 @@
+const blogs = [
+  {
+    "id": "1",
+    "title": "SSR vs SSG",
+    "date": "Oct 1, 2021",
+    "category": "Computer",
+    "tags": "ReactJS,NextJS,JS,SSG,Test3",
+    "imageLink": "https://source.unsplash.com/m_HRfLhgABo/1600x900",
+    "featured": "1",
+    "filename": "ssg-ssr"
+  }
+]
+
+export default blogs
--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { mapAllBlogs } from '../../lib/blog'
-
+import Blogs from '../../content/blog'
 
 export default function handler(req, res) {
-    res.status(200).json(mapAllBlogs())
+    res.status(200).json(Blogs)
 }
